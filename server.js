f8fabc00a451625058fe6d6a4ae46fc5ffacd6ec
dev-ale <ale.iphone@gmail.com>
2021-04-13 14:54:43 +0200

    initial

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use("/", serveStatic(path.join(__dirname, '/dist')))
 app.get(/.*/, function (req, res) {
     res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 5000;
 app.listen(port)
 console.log(`App is listening on port: ${port}`)
