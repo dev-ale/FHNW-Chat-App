f605af6745e98589d1f8c4fd7e2c214817496f0b
dev-ale <ale.iphone@gmail.com>
2021-04-21 14:53:08 +0200

    added some example about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,32 @@
   <v-container>
     <v-card height="700">
       <v-card-text>
-        <v-card-title>about</v-card-title>
+        <v-card-title>
+          <v-spacer></v-spacer>about
+        <v-spacer></v-spacer></v-card-title>
+        <v-row>
+          <v-col>
+            <h2>Technologien</h2>
+            <v-list>
+              <v-list-item>Vue.js - Frontend</v-list-item>
+              <v-list-item>Node.js(Express) - Backend</v-list-item>
+              <v-list-item>MongoDB Atlas - Datenbank</v-list-item>
+              <v-list-item>Heroku - Deployment</v-list-item>
+            </v-list>
+          </v-col>
+          <v-col>
+            <h2>Funktionen</h2>
+            <v-list>
+              <v-list-item>Registrieren</v-list-item>
+              <v-list-item>Einloggen</v-list-item>
+              <v-list-item>Verificication</v-list-item>
+              <v-list-item>protected routes</v-list-item>
+              <v-list-item>https</v-list-item>
+              <v-list-item>REST api requests</v-list-item>
+
+            </v-list>
+          </v-col>
+        </v-row>
       </v-card-text>
     </v-card>
   </v-container>
