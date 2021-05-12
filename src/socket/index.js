18332372a724694264538be1299f40d764a4d848
David Kern <david.kern1@students.fhnw.ch>
2021-05-12 16:06:17 +0200

    change hostname socket server

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,6 +1,8 @@
 //socket connection
 import io from 'socket.io-client'
 
-const socket = io("http://localhost:5000", { withCredentials: true, extraHeaders: { "Access-Control-Allow-Credentials": "Yes" } })
+//const socket = io("http://localhost:5000", { withCredentials: true, extraHeaders: { "Access-Control-Allow-Credentials": "Yes" } })
+const socket = io('${window.location.hostname}:5000', { withCredentials: true, extraHeaders: { "Access-Control-Allow-Credentials": "Yes" } })
+
 
 export default socket
