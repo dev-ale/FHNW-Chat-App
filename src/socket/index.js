018a6edc6108b5fc89a7712e4e13cb2bea643a4a
David Kern <david.kern1@students.fhnw.ch>
2021-05-12 16:11:34 +0200

    socket change url

--- src/socket/index.js
+++ src/socket/index.js
@@ -2,7 +2,7 @@
 import io from 'socket.io-client'
 
 //const socket = io("http://localhost:5000", { withCredentials: true, extraHeaders: { "Access-Control-Allow-Credentials": "Yes" } })
-const socket = io('${window.location.hostname}:5000', { withCredentials: true, extraHeaders: { "Access-Control-Allow-Credentials": "Yes" } })
+const socket = io("https://web-engineering-fhnw.herokuapp.com:5000", { withCredentials: true, extraHeaders: { "Access-Control-Allow-Credentials": "Yes" } })
 
 
 export default socket
