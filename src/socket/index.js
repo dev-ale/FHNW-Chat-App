72dc117fdf395a8c951a91bc00f5678ec278c147
David Kern <david.kern1@students.fhnw.ch>
2021-05-12 16:19:00 +0200

    heruko config

--- src/socket/index.js
+++ src/socket/index.js
@@ -2,7 +2,7 @@
 import io from 'socket.io-client'
 
 //const socket = io("http://localhost:5000", { withCredentials: true, extraHeaders: { "Access-Control-Allow-Credentials": "Yes" } })
-const socket = io("https://web-engineering-fhnw.herokuapp.com:5000", { withCredentials: true, extraHeaders: { "Access-Control-Allow-Credentials": "Yes" } })
+const socket = io(process.env.URL_SOCKET + ":5000", { withCredentials: true, extraHeaders: { "Access-Control-Allow-Credentials": "Yes" } })
 
 
 export default socket
