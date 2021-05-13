98b6fb8a957a7666efb8a49c2ad23a51aa24644c
David Kern <david.kern1@students.fhnw.ch>
2021-05-13 21:01:12 +0200

    port change socket

--- src/socket/index.js
+++ src/socket/index.js
@@ -2,7 +2,7 @@
 import io from 'socket.io-client'
 
 //const socket = io("http://localhost:5000", { withCredentials: true, extraHeaders: { "Access-Control-Allow-Credentials": "Yes" } })
-const socket = io(process.env.VUE_APP_URL_SOCKET + ":5000", { withCredentials: true, extraHeaders: { "Access-Control-Allow-Credentials": "Yes" } })
+const socket = io(process.env.VUE_APP_URL_SOCKET, { withCredentials: true, extraHeaders: { "Access-Control-Allow-Credentials": "Yes" } })
 
 
 export default socket
